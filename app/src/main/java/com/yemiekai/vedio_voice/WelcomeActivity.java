e8c9ac47d39b5b3d33435cc163a828cc528f9385
yemiekai <418504286@qq.com>
2019-06-18 20:21:54 +0800

    add welcome activity

--- app/src/main/java/com/yemiekai/vedio_voice/WelcomeActivity.java
+++ app/src/main/java/com/yemiekai/vedio_voice/WelcomeActivity.java
@@ -10,13 +10,18 @@ import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.VideoView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.yemiekai.vedio_voice.utils.StringUtils.print;
 
@@ -25,6 +30,11 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private Activity context = null;
     private boolean bVideoComplete = false;
+
+    private TextView tx_time;
+    private TextView tx_date;
+    private ConstraintLayout layout2;
+    private Timer timeTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +45,6 @@ public class WelcomeActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
 
-        final ConstraintLayout layout = context.findViewById(R.id.welcome_layout);
         final VideoView vv = (VideoView) this.findViewById(R.id.welcome_video);
 
         final String uri_video = "android.resource://" + getPackageName() + "/" + R.raw.welcome_vedio;
@@ -48,29 +57,52 @@ public class WelcomeActivity extends AppCompatActivity {
                 mp.setLooping(false);
             }
         });
-
         vv.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
-                public void onCompletion(MediaPlayer mp) {
-                layout.setBackgroundResource(R.drawable.welcome_image);  // 显示欢迎图片
-                vv.setVisibility(View.INVISIBLE);  // 隐藏视频控件
+            public void onCompletion(MediaPlayer mp) {
                 bVideoComplete = true;
+                dispayWishPage();  // 视频播放完毕， 显示祝福页
             }
         });
-
-
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        print("event... keyDown");
-
+        // 视频放完了才能响应遥控器
         if(bVideoComplete){
             Intent intent = new Intent(context, MainActivity.class);
             startActivity(intent);
             context.finish();
         }
-
         return super.onKeyDown(keyCode, event);
     }
+
+    // 显示祝福页
+    public void dispayWishPage(){
+        setContentView(R.layout.activity_welcome2);
+        layout2 = (ConstraintLayout) this.findViewById(R.id.welcome2_layout);
+        tx_time = (TextView) this.findViewById(R.id.welecome2_time);
+        tx_date = (TextView) this.findViewById(R.id.welecome2_date);
+
+        timeTimer = new Timer();
+        timeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                long sysTime = System.currentTimeMillis();//获取系统时间
+                final CharSequence sysDateStr = DateFormat.format("yyyy/MM/dd E", sysTime);
+                final CharSequence sysTimeStr = DateFormat.format("HH:mm", sysTime);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tx_time.setText(sysTimeStr);
+                        tx_date.setText(sysDateStr);
+                    }
+                });
+            }
+        },0,1000);
+
+        layout2.setBackgroundResource(R.drawable.welcome_image);  // 显示欢迎图片
+    }
+
+
 }
