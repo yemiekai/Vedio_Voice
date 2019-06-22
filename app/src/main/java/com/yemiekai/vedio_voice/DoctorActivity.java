5d95e1d83a3265783e91eff3ba1eee629a471be1
yemiekai <418504286@qq.com>
2019-06-22 11:42:04 +0800

    添加医生信息

---
+++ app/src/main/java/com/yemiekai/vedio_voice/DoctorActivity.java
@@ -0,0 +1,17 @@
+package com.yemiekai.vedio_voice;
+
+/**
+ * (1)ExpandableListView
+ *
+ * (2)Android UI 之实现多级列表TreeView
+ * https://blog.csdn.net/u013749540/article/details/59113064
+ *
+ * (3)Android使用RecyclerView实现树形列表升级版
+ * https://blog.csdn.net/q1113225201/article/details/79328247
+ *
+ * (4)Android 多级菜单联动操作
+ * https://blog.csdn.net/hzy670800844/article/details/62037033
+ */
+
+public class DoctorActivity {
+}
